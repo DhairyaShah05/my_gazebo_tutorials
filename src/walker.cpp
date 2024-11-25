044d55b06c1c3467baf44b6f4faec08dc57f2d8b
Dhairya Shah <dshah05@umd.edu>
2024-11-24 23:41:45 -0500

    Updated parameters and Added Rosbag File

--- src/walker.cpp
+++ src/walker.cpp
@@ -10,10 +10,10 @@
  */
 #include "/home/dhairya/ws2/my_gazebo_tutorials/src/walker/include/walker.hpp"
 
-#define MAX_FORWARD_SPEED 0.5
-#define MAX_ROTATION_SPEED 0.5
-#define DISTANCE_THRESHOLD 1.0
-#define ROTATION_DISTANCE_THRESHOLD 1.0
+#define MAX_FORWARD_SPEED 0.1
+#define MAX_ROTATION_SPEED 0.3
+#define DISTANCE_THRESHOLD 0.5
+#define ROTATION_DISTANCE_THRESHOLD 1.5
 
 // Walker class definition
 Walker::Walker() : Node("walker") {
